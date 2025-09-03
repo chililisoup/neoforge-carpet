187751eef17de185198d7ad94bff0b6c6ebe3c26
chililisoup <rebot333gaming@gmail.com>
2025-09-03 15:31:11 -0600

    piston adjustments
    (cherry picked from commit eddb9d6226060b1f4e87f790f161fffa095b46e7)

--- src/main/java/carpet/mixins/PistonStructureResolver_customStickyMixin.java
+++ src/main/java/carpet/mixins/PistonStructureResolver_customStickyMixin.java
@@ -7,10 +7,7 @@ import com.llamalad7.mixinextras.sugar.Local;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -24,23 +21,6 @@ public class PistonStructureResolver_customStickyMixin {
     @Shadow @Final private Level level;
     @Shadow @Final private Direction pushDirection;
 
-    // fields that are needed because @Redirects cannot capture locals
-    @Unique private BlockPos pos_addBlockLine;
-    @Unique private BlockPos behindPos_addBlockLine;
-
-    @Inject(
-        method = "addBlockLine",
-        at = @At(
-                value = "INVOKE",
-                ordinal = 1,
-                target = "Lnet/minecraft/world/level/Level;getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;"
-        )
-    )
-    private void captureBlockLinePositions(BlockPos pos, Direction fromDir, CallbackInfoReturnable<Boolean> cir, @Local(ordinal = 1) BlockPos behindPos) {
-        pos_addBlockLine = behindPos.relative(pushDirection);
-        behindPos_addBlockLine = behindPos;
-    }
-
     @WrapOperation(
         method = "addBlockLine",
         at = @At(
@@ -49,9 +29,22 @@ public class PistonStructureResolver_customStickyMixin {
             ordinal = 0
         )
     )
-    private boolean onAddBlockLineCanStickToEachOther(BlockState state, BlockState behindState, Operation<Boolean> original) {
+    private boolean onAddBlockLineCanStickToEachOther(
+            BlockState state,
+            BlockState behindState,
+            Operation<Boolean> original,
+            @Local(ordinal = 1) BlockPos behindPos
+    ) {
         if (state.getBlock() instanceof BlockPistonBehaviourInterface behaviourInterface) {
-            return behaviourInterface.isStickyToNeighbor(level, pos_addBlockLine, state, behindPos_addBlockLine, behindState, pushDirection.getOpposite(), pushDirection);
+            return behaviourInterface.isStickyToNeighbor(
+                    level,
+                    behindPos.relative(pushDirection),
+                    state,
+                    behindPos,
+                    behindState,
+                    pushDirection.getOpposite(),
+                    pushDirection
+            );
         }
 
         return original.call(state, behindState);
@@ -69,23 +62,6 @@ public class PistonStructureResolver_customStickyMixin {
         return true;
     }
 
-    // fields that are needed because @Redirects cannot capture locals
-    @Unique private Direction dir_addBranchingBlocks;
-    @Unique private BlockPos neighborPos_addBranchingBlocks;
-
-    @Inject(
-        method = "addBranchingBlocks",
-        at = @At(
-                value = "INVOKE",
-                ordinal = 1,
-                target = "Lnet/minecraft/world/level/Level;getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;"
-        )
-    )
-    private void captureNeighborPositions(BlockPos pos, CallbackInfoReturnable<Boolean> cir, @Local Direction dir, @Local(ordinal = 1) BlockPos neighborPos) {
-        dir_addBranchingBlocks = dir;
-        neighborPos_addBranchingBlocks = neighborPos;
-    }
-
     @WrapOperation(
         method = "addBranchingBlocks",
         at = @At(
@@ -94,9 +70,16 @@ public class PistonStructureResolver_customStickyMixin {
             ordinal = 0
         )
     )
-    private boolean onAddBranchingBlocksCanStickToEachOther(BlockState neighborState, BlockState state, Operation<Boolean> original, @Local(argsOnly = true) BlockPos pos) {
+    private boolean onAddBranchingBlocksCanStickToEachOther(
+            BlockState neighborState,
+            BlockState state,
+            Operation<Boolean> original,
+            @Local(argsOnly = true) BlockPos pos,
+            @Local(ordinal = 1) BlockPos neighborPos,
+            @Local Direction direction
+    ) {
         if (state.getBlock() instanceof BlockPistonBehaviourInterface behaviourInterface) {
-            return behaviourInterface.isStickyToNeighbor(level, pos, state, neighborPos_addBranchingBlocks, neighborState, dir_addBranchingBlocks, pushDirection);
+            return behaviourInterface.isStickyToNeighbor(level, pos, state, neighborPos, neighborState, direction, pushDirection);
         }
 
         return original.call(neighborState, state);
